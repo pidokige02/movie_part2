818f44cc581f31cca7ec231fb484e047959eaa0d
pidokige02 <pidokige0204@gmail.com>
2022-06-11 15:13:51 +0900

    2nd commit w/ css

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import  Movie  from "../components/Movie"
+import styles from "./Home.module.css";
 
 
 function Home () {
@@ -19,21 +20,24 @@ function Home () {
     }, []);
 
     return (
-      <div>
+      <div className={styles.container}>
         {loading ? (
-          <h1>Loading...</h1>
+          <div className={styles.loader}>
+            <span>Loading...</span>
+          </div>
         ) : (
-          <div>
-            { movies.map((movie) => (
+          <div className={styles.movies}>
+            {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
+                year={movie.year}
                 coverImg={movie.medium_cover_image}
-                title={movie.title }
+                title={movie.title}
                 summary={movie.summary}
                 genres={movie.genres}
               />
-              ))}
+            ))}
           </div>
         )}
       </div>
